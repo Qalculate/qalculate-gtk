180c3fc7bb802315446062e40de0e9966063a62e
Hanna K <hanna_k@fmgirl.com>
2016-12-15 11:50:27 +0100

    Revert "Replace is_number function with parsed_mstruct->isNumber()"
    This reverts commit fc210763ed083fab83660ef8e2f2bfc87a1cfbb4.

--- src/callbacks.cc
+++ src/callbacks.cc
@@ -8861,6 +8861,14 @@ void new_vector(GtkMenuItem*, gpointer)
 	edit_matrix(_("Vectors"), NULL, NULL, GTK_WIDGET(gtk_builder_get_object(main_builder, "main_window")), TRUE);
 }
 
+bool is_number(const gchar *str) {
+	while (*str) {
+		if ((*str < '0' || *str > '9') && *str != '.') return false;
+		str++;
+	}
+	return true;
+}
+
 /*
 	insert one-argument function when button clicked
 */
@@ -8869,7 +8877,7 @@ void insertButtonFunction(const gchar *text, bool append_space = true) {
 	const gchar *expr = gtk_entry_get_text(GTK_ENTRY(expression));
 	int old_length = g_utf8_strlen(expr, -1);
 	// special case: the user just entered a number, then select all, so that it gets executed
-	if(parsed_mstruct && parsed_mstruct->isNumber()) {
+	if (is_number(expr)) {
 		gtk_editable_select_region(GTK_EDITABLE(expression), 0, old_length);
 	}
 	if(gtk_editable_get_selection_bounds(GTK_EDITABLE(expression), &start, &end)) {
