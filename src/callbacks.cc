542d16e33be9b994e160d4e41865930f190f1600
Hanna K <hanna_k@fmgirl.com>
2017-06-28 20:11:13 +0200

    Updates for new calculation termination and factorization in libqalculate

--- src/callbacks.cc
+++ src/callbacks.cc
@@ -5586,12 +5586,23 @@ void setResult(Prefix *prefix, bool update_history, bool update_parse, bool forc
 }
 
 void on_abort_command(GtkDialog*, gint, gpointer) {
-	command_thread->cancel();
-	CALCULATOR->restoreState();
-	CALCULATOR->clearBuffers();
-	b_busy = false;
-	b_busy_command = false;
-	command_aborted = true;
+	CALCULATOR->abortCalculation();
+	struct timespec rtime;
+	rtime.tv_sec = 0;
+	rtime.tv_nsec = 1000000;
+	int msecs = 250;
+	while(b_busy && msecs > 0) {
+		nanosleep(&rtime, NULL);
+		msecs--;
+	}
+	if(b_busy) {
+		command_thread->cancel();
+		CALCULATOR->restoreState();
+		CALCULATOR->clearBuffers();
+		b_busy = false;
+		b_busy_command = false;
+		command_aborted = true;
+	}
 }
 
 void CommandThread::run() {
@@ -5601,10 +5612,11 @@ void CommandThread::run() {
 	while(true) {
 		int command_type = read<int>();
 		void *x = read<void *>();
+		CALCULATOR->startCalculationControl();
 		switch(command_type) {
 			case COMMAND_FACTORIZE: {
 				if(!((MathStructure*) x)->integerFactorize()) {
-					((MathStructure*) x)->factorize(evalops, true, true, true);
+					((MathStructure*) x)->factorize(evalops, true, -1, 0);
 				}
 				break;
 			}
@@ -5613,6 +5625,7 @@ void CommandThread::run() {
 				break;
 			}
 		}
+		CALCULATOR->stopCalculationControl();
 		b_busy = false;
 		
 	}
