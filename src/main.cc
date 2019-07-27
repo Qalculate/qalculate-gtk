75fb019d536e5657bc037b15f3828ac4f1af530b
Hanna K <hanna.knutsson@protonmail.com>
2019-07-27 16:53:00 +0200

    Fix segfault with command line expression (issue #107)

--- src/main.cc
+++ src/main.cc
@@ -131,9 +131,7 @@ void create_application(GtkApplication *app) {
 
 	showing_first_time_message = first_time;
 
-	if(!calc_arg.empty()) {
-		gtk_text_buffer_set_text(GTK_TEXT_BUFFER(gtk_builder_get_object(main_builder, "expressionbuffer")), calc_arg.c_str(), -1);
-	} else if(first_time) {		
+	if(calc_arg.empty() && first_time) {
 		PangoLayout *layout = gtk_widget_create_pango_layout(GTK_WIDGET(gtk_builder_get_object(main_builder, "resultview")), NULL);
 		GdkRGBA rgba;
 #if GDK_MAJOR_VERSION > 3 || GDK_MINOR_VERSION >= 22
@@ -246,6 +244,7 @@ void create_application(GtkApplication *app) {
 	command_thread = new CommandThread;
 	
 	if(!calc_arg.empty()) {
+		gtk_text_buffer_set_text(GTK_TEXT_BUFFER(gtk_builder_get_object(main_builder, "expressionbuffer")), calc_arg.c_str(), -1);
 		execute_expression();
 	} else if(!first_time) {
 		int base = printops.base;
