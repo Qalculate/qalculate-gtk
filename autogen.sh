a2df2ac0929ce55a92ae0f8a7da1c75b8a115fa7
Đoàn Trần Công Danh <congdanhqx@gmail.com>
2025-11-21 07:24:13 +0700

    translation: Do not translation releases

--- autogen.sh
+++ autogen.sh
@@ -3,4 +3,5 @@ libtoolize --force --copy \
 && aclocal \
 && autoheader \
 && automake --add-missing \
+&& sed '/<releases>/,/<\/releases>/d' data/qalculate-gtk.appdata.xml.in >data/qalculate-gtk.noinst.appdata.xml \
 && autoconf
